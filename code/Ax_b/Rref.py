2391461066d373a4f21bfbe3bb8850a96348acbc
Leon <52199211+bboyGit@users.noreply.github.com>
2019-07-25 21:49:13 +0800

    Update Rref.py

--- code/Ax_b/Rref.py
+++ code/Ax_b/Rref.py
@@ -2,6 +2,7 @@ import numpy as np
 from code.Ax_b.LU import LU_decompose
 from code.Ax_b.Inverse import inverse
 
+
 def rref(mat):
     # args:
     #   mat: A matrix prepared to become simplest row echelon form matrix.
@@ -11,13 +12,14 @@ def rref(mat):
     lu = LU_decompose(mat)
     upper = lu['upper']
 
-    # (2)from upper triangular to simplest_row_echelon_matrix
+    # (2)from upper triangular to simplest row echelon matrix
     try:
         # If the upper triangular is non-singular, then it can be reduced to a identity matrix.
         inv = inverse(upper)
         ref = np.identity(mat.shape[0])
-        
+
     except:
+        elementary = []
         pivot_row, pivot_col = upper.shape
         pivot_row -= 1
         pivot_col -= 1
@@ -25,10 +27,14 @@ def rref(mat):
         for i in range(pivot_row):
             idx = np.where(upper[i, :] != 0)[0]
             if idx.shape[0] == 0:
+                # All of this row in upper are zero
                 continue
             else:
                 first_idx = idx[0]
-                upper[i, :] = upper[i, :]/upper[i, first_idx]
+                E = np.identity(upper.shape[0])
+                E[i, i] = 1/upper[i, first_idx]
+                upper = E @ upper
+                elementary.append(E)
 
         # (4) Use elimination to get the simplest row echelon form matrix
         def row_subtract(matrix, nrow, nonZero_idx):
@@ -37,6 +43,7 @@ def rref(mat):
             multiplier = matrix[:nrow, [nonZero_idx]] / matrix[nrow, nonZero_idx]
             E[:nrow, [nrow]] = - multiplier
             matrix = E @ matrix
+            elementary.append(E)
 
             return matrix
 
@@ -49,5 +56,8 @@ def rref(mat):
                 first_idx = idx[0]
                 ref = row_subtract(ref, i, first_idx)
 
+    return {"rref": ref, 'elementary': elementary}
 
-    return ref
+if __name__ == "__main__":
+    mat = np.array([[1, 3, 3, 2], [2, 6, 9, 7], [-1, -3, 3, 4]])
+    r = rref(mat)
