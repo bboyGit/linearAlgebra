80cfc61d6549f48f2c2e3fcdfdf90d8619fe1bca
Leon <52199211+bboyGit@users.noreply.github.com>
2019-07-25 21:49:56 +0800

    Add files via upload
    Calculate rank of a matrix

---
+++ code/Ax_b/Rank.py
@@ -0,0 +1,17 @@
+import numpy as np
+from core.Ax_b.nullSpace import getNullSpace
+
+def rank(mat):
+    # args:
+    #   mat: A matrix
+    # return: the rank of matrix
+
+    col_null = getNullSpace(mat)
+    colspace = col_null['column_space']
+    result = colspace.shape[1]
+
+    return result
+
+if __name__ == "__main__":
+    mat = np.array([[1, 3, 3, 2], [2, 6, 9, 7], [-1, -3, 3, 4]])
+    r = rank(mat)
