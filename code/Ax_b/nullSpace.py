715b5cbc2a03084fb6160d9c1bd88653d1e2ceb2
bboyGit <52199211+bboyGit@users.noreply.github.com>
2019-07-25 10:44:06 +0800

    Update nullSpace.py
    calculate the column space and null space of a given matrix.

--- code/Ax_b/nullSpace.py
+++ code/Ax_b/nullSpace.py
@@ -7,10 +7,10 @@ def getNullSpace(mat):
     #   mat: A matrix
     # return: A matrix whose columns are the basis of null space of mat
 
-    # (1) get the simplest reduced row echelon form matrix
+    # (1) Get the simplest reduced row echelon form matrix
     ref = rref(mat)
 
-    # (2) find column space and null space of ref
+    # (2) Find column space and null space of ref
     nrow, ncol = ref.shape
     if nrow == ncol and (ref == np.identity(ref.shape[0])).all():
         colspace = ref
@@ -19,10 +19,11 @@ def getNullSpace(mat):
     else:
         col_bool = []
         df = pd.DataFrame(ref)
+        # (2.1) Get the columns index of pivot_variable and free_variable
         for col in df:
             the_col = df[col]
-            cond1 = set(the_col.unique()) == set([1,0])
-            cond2 = the_col[the_col].shape[0] == 1
+            cond1 = set(the_col.unique()) == set([1, 0])
+            cond2 = the_col[the_col == 1].shape[0] == 1
             if col == 0 and cond1 and cond2:
                 col_bool.append(True)
             elif col != 0 and cond1 and cond2:
@@ -36,17 +37,39 @@ def getNullSpace(mat):
         pivot_col = list(col_bool[col_bool].index)
         free_col = list(col_bool[~col_bool].index)
 
+        # (2.2) Iter each free variable to set it to 1 and other free variables to 0.
+
         for sp_idx in range(len(free_col)):
             pivot_free1 = [1 if i in pivot_col or i == free_col[sp_idx] else 0 for i in range(df.shape[1])]
-            make_zero = np.array([pivot_free1]).T
-            r = ref @ make_zero
+            make_zero = np.array([pivot_free1])
+            r = np.multiply(ref, make_zero)
+            drop_all_zero_row = [False if (r[i, :] == 0).all() else True for i in range(r.shape[0])]
+            r = r[drop_all_zero_row, :]
 
+            free_solution = {}
+            for idx, value in enumerate(free_col):
+                if idx == sp_idx:
+                    free_solution[value] = 1
+                else:
+                    free_solution[value] = 0
+            free_solution = pd.DataFrame(free_solution, index=['solution' + str(sp_idx + 1)])
 
+            pivot_solution = {}
+            for j in range(r.shape[0]):
+                this_row = r[j, :]
+                free_var_col_nonzero = free_col[sp_idx]
+                free = this_row[free_var_col_nonzero]
+                pivot = this_row[this_row != 0][0]
+                pivot_idx = np.where(this_row != 0)[0][0]
+                pivot_solution[pivot_idx] = - free/pivot
+            pivot_solution = pd.DataFrame(pivot_solution, index=['solution' + str(sp_idx + 1)])
 
+            special_solution = pd.concat([pivot_solution, free_solution], axis=1)
+            special_solution = special_solution.reindex(columns=range(special_solution.shape[1]))
 
-    return nullspace
+            if 'nullspace' not in locals():
+                nullspace = special_solution.T
+            else:
+                nullspace = pd.concat([nullspace, special_solution.T], axis=1)
 
-
-if __name__ == "__main__":
-
-    nullspace = getNullSpace(mat)
+    return {'null_space': nullspace, 'column_space': colspace}
