d73c3848616b3e27b0c252a3df6544e4345f02cf
Leon <52199211+bboyGit@users.noreply.github.com>
2019-07-29 20:48:42 +0800

    Add files via upload
    decide whether the column vectors in a matrix are linearly independent.

---
+++ code/Ax_b/linearDependent.py
@@ -0,0 +1,18 @@
+import numpy as np
+from core.Ax_b.nullSpace import getNullSpace
+
+def linearDependent(mat):
+    # Desc: check whether the coluumns of mat are linearly independent.
+    # return: A bool, True indicating linear independent.
+    nrow, ncol = mat.shape
+    if ncol > nrow:
+        return False
+    else:
+        col_null = getNullSpace(mat)
+        nullpace = np.array(col_null['null_space'])
+        cond = nullpace == np.array([[0] * ncol]).T
+        if cond.all():
+            return True
+        else:
+            return False
+
