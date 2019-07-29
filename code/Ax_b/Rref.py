24872bcf2a12aa234dd149518d44836ff52b2d1a
Leon <52199211+bboyGit@users.noreply.github.com>
2019-07-29 19:07:41 +0800

    Update Rref.py
    modified a latent bug.

--- code/Ax_b/Rref.py
+++ code/Ax_b/Rref.py
@@ -2,7 +2,6 @@ import numpy as np
 from code.Ax_b.LU import LU_decompose
 from code.Ax_b.Inverse import inverse
 
-
 def rref(mat):
     # args:
     #   mat: A matrix prepared to become simplest row echelon form matrix.
@@ -13,16 +12,27 @@ def rref(mat):
     upper = lu['upper']
 
     # (2)from upper triangular to simplest row echelon matrix
+    def row_subtract(matrix, nrow, nonZero_idx):
+
+        E = np.identity(matrix.shape[0])
+        multiplier = matrix[:nrow, [nonZero_idx]] / matrix[nrow, nonZero_idx]
+        E[:nrow, [nrow]] = - multiplier
+        matrix = E @ matrix
+        elementary.append(E)
+
+        return matrix
+    pivot_row, pivot_col = upper.shape
+    elementary = []
     try:
         # If the upper triangular is non-singular, then it can be reduced to a identity matrix.
         inv = inverse(upper)
-        ref = np.identity(mat.shape[0])
+        ref = upper.copy()
+        for i in range(1, pivot_row):
+            idx = np.where(upper[i, :] != 0)[0]
+            first_idx = idx[0]
+            ref = row_subtract(ref, i, first_idx)
 
-    except:
-        elementary = []
-        pivot_row, pivot_col = upper.shape
-        pivot_row -= 1
-        pivot_col -= 1
+    except Exception:
         # (3) let each non-zero pivot to be 1.
         for i in range(pivot_row):
             idx = np.where(upper[i, :] != 0)[0]
@@ -37,16 +47,6 @@ def rref(mat):
                 elementary.append(E)
 
         # (4) Use elimination to get the simplest row echelon form matrix
-        def row_subtract(matrix, nrow, nonZero_idx):
-
-            E = np.identity(matrix.shape[0])
-            multiplier = matrix[:nrow, [nonZero_idx]] / matrix[nrow, nonZero_idx]
-            E[:nrow, [nrow]] = - multiplier
-            matrix = E @ matrix
-            elementary.append(E)
-
-            return matrix
-
         ref = upper.copy()
         for i in range(1, pivot_row):
             idx = np.where(upper[i, :] != 0)[0]
@@ -57,7 +57,3 @@ def rref(mat):
                 ref = row_subtract(ref, i, first_idx)
 
     return {"rref": ref, 'elementary': elementary}
-
-if __name__ == "__main__":
-    mat = np.array([[1, 3, 3, 2], [2, 6, 9, 7], [-1, -3, 3, 4]])
-    r = rref(mat)
