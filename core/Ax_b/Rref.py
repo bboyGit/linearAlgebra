6be8bfc2e5da1e36a3c55297a7bc3c06ee9cc203
Leon <52199211+bboyGit@users.noreply.github.com>
2019-08-04 22:27:02 +0800

    Update and rename code/Ax_b/Rref.py to core/Ax_b/Rref.py

--- core/Ax_b/Rref.py
+++ core/Ax_b/Rref.py
@@ -1,6 +1,6 @@
 import numpy as np
-from code.Ax_b.LU import LU_decompose
-from code.Ax_b.Inverse import inverse
+from core.Ax_b.LU import LU_decompose
+from core.Ax_b.Inverse import inverse
 
 def rref(mat):
     # args:
